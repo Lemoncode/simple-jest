9a2cff4f4b8bcf5a56ee2c730b18cbe5452872fb
Braulio Diez Botella <braulio.diez@gmail.com>
2019-03-31 19:28:57 +0200

    sample completed

---
+++ 04 hooks/01 implemented/readme.md
@@ -0,0 +1,17 @@
+# Intro
+
+In this sample we are going to test a component that is using hooks.
+
+# Steps
+
+We will start from sample _00 start_. Let's copy the content and execute npm install.
+
+```bash
+npm install
+```
+
+- We are going to test _./src/nameEdit.tsx_, let's create a test file called
+  _nameEdit.spec.tsx_
+
+```typescript
+```
---
+++ 04 hooks/01 implemented/src/nameEdit.spec.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { NameEditComponent } from "./nameEdit";
+import { shallow } from "enzyme";
+import { act } from "react-dom/test-utils"; // ES6
+
+describe("NameEditComponent", () => {
+  it("It should update state on OnChange", () => {
+    // Arrange
+    const props = {
+      initialUserName: "",
+      onNameUpdated: jest.fn()
+    };
+
+    /*
+     * Wrap any code rendering and triggering updates to your components into `act()` calls.
+     *
+     * Ensures that the behavior in your tests matches what happens in the browser
+     * more closely by executing pending `useEffect`s before returning. This also
+    * reduces the amount of re-renders done.
+    */
+    // Act
+    const component = shallow(<NameEditComponent {...props} />);
+
+    act(() => {
+      component.find("input").simulate("change", {
+        target: {
+          value: "updated"
+        }
+      });  
+    })
+
+    component.find("button").simulate("click");
+
+    // Assert
+    expect(props.onNameUpdated).toHaveBeenCalledWith("updated");
+  });
+});
